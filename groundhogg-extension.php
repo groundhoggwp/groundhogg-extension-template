786c8079688b5eba4db635f347b72af717959f21
tobeyadr <adrian@trainingbusinesspros.com>
2020-05-01 15:57:47 -0400

    new template file for checking Gorundhogg core version

--- groundhogg-extension.php
+++ groundhogg-extension.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * Plugin Name: Groundhogg - Extension Name
+ * Plugin Name: Groundhogg - Extension
  * Plugin URI:  https://www.groundhogg.io/?utm_source=wp-plugins&utm_campaign=plugin-uri&utm_medium=wp-dash
- * Description: The description of your extension.
- * Version: 2.0
+ * Description: Your extension description here
+ * Version: 1.0
  * Author: Groundhogg Inc.
  * Author URI: https://www.groundhogg.io/?utm_source=wp-plugins&utm_campaign=author-uri&utm_medium=wp-dash
  * Text Domain: groundhogg
@@ -19,14 +19,15 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  */
+namespace GroundhoggExtension;
 
-// TODO Refactor `groundhogg_extension`, `GroundhoggExtension` and `GROUNDHOGG_EXTENSION` with the name of your extension to avoid conflicts.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-define( 'GROUNDHOGG_EXTENSION_VERSION', '1.0' ); // Todo Update Version #
-define( 'GROUNDHOGG_EXTENSION_PREVIOUS_STABLE_VERSION', '0.1' ); // TODO  Update Version #
-define( 'GROUNDHOGG_EXTENSION_NAME', 'My Extension' ); //TODO Update the name of your extension.
+define( 'GROUNDHOGG_EXTENSION_VERSION', '1.0' );
+define( 'GROUNDHOGG_EXTENSION_PREVIOUS_STABLE_VERSION', '2.3.4' );
+define( 'GROUNDHOGG_EXTENSION_NAME', 'Extnesion Name' );
 
 define( 'GROUNDHOGG_EXTENSION__FILE__', __FILE__ );
 define( 'GROUNDHOGG_EXTENSION_PLUGIN_BASE', plugin_basename( GROUNDHOGG_EXTENSION__FILE__ ) );
@@ -37,48 +38,112 @@ define( 'GROUNDHOGG_EXTENSION_URL', plugins_url( '/', GROUNDHOGG_EXTENSION__FILE
 define( 'GROUNDHOGG_EXTENSION_ASSETS_PATH', GROUNDHOGG_EXTENSION_PATH . 'assets/' );
 define( 'GROUNDHOGG_EXTENSION_ASSETS_URL', GROUNDHOGG_EXTENSION_URL . 'assets/' );
 
-add_action( 'plugins_loaded', function (){
-    load_plugin_textdomain( GROUNDHOGG_EXTENSION_TEXT_DOMAIN, false, basename( dirname( __FILE__ ) ) . '/languages' );
+define( 'GROUNDHOGG_EXTENSION_REQUIRED_WP_VERSION', '4.9' );
+define( 'GROUNDHOGG_EXTENSION_REQUIRED_PHP_VERSION', '7.0' );
+define( 'GROUNDHOGG_EXTENSION_REQUIRED_CORE_VERSION', '2.1.14' );
+
+add_action( 'plugins_loaded', function () {
+	load_plugin_textdomain( GROUNDHOGG_EXTENSION_TEXT_DOMAIN, false, basename( dirname( __FILE__ ) ) . '/languages' );
 } );
 
 define( 'GROUNDHOGG_EXTENSION_TEXT_DOMAIN', 'groundhogg' );
 
-if ( ! version_compare( PHP_VERSION, '5.6', '>=' ) ) {
-    add_action( 'admin_notices', function(){
-        $message = sprintf( esc_html__( '%s requires PHP version %s+, plugin is currently NOT RUNNING.', 'groundhogg' ), GROUNDHOGG_EXTENSION_NAME, '5.6' );
-        $html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
-        echo wp_kses_post( $html_message );
-    } );
-} elseif ( ! version_compare( get_bloginfo( 'version' ), '4.9', '>=' ) ) {
-    add_action( 'admin_notices', function (){
-        $message = sprintf( esc_html__( '%s requires WordPress version %s+. Because you are using an earlier version, the plugin is currently NOT RUNNING.', 'groundhogg' ), GROUNDHOGG_EXTENSION_NAME, '4.9' );
-        $html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
-        echo wp_kses_post( $html_message );
-    } );
-} else {
+// Check PHP and WP are up to date!
+if ( check_wp_version() && check_php_version() ){
 
-    // Groundhogg is loaded, load now.
-    if ( did_action( 'groundhogg/loaded' ) ){
+	// Groundhogg is loaded, load now.
+	if ( did_action( 'groundhogg/loaded' ) ) {
 
-        require GROUNDHOGG_EXTENSION_PATH . 'includes/plugin.php';
+		if ( check_core_version() ){
+			require __DIR__ . '/includes/plugin.php';
+		}
 
-    // Lazy load, wait for Groundhogg!
-    } else {
-        add_action('groundhogg/loaded', function () {
-            require GROUNDHOGG_EXTENSION_PATH . 'includes/plugin.php';
-        });
+		// Lazy load, wait for Groundhogg!
+	} else {
 
-        // Might not actually be loaded, so we'll check in later.
-        add_action( 'admin_notices', function () {
+		add_action( 'groundhogg/loaded', function () {
+			if ( check_core_version() ){
+				require __DIR__ . '/includes/plugin.php';
+			}
+		} );
 
-            // Is not loaded!
-            if ( ! defined( 'GROUNDHOGG_VERSION' ) ){
-                $message = sprintf(esc_html__('Groundhogg is not currently active, it must be active for %s to work.', 'groundhogg'), GROUNDHOGG_EXTENSION_NAME );
-                $html_message = sprintf('<div class="notice notice-warning">%s</div>', wpautop($message));
-                echo wp_kses_post($html_message);
-            }
-        });
-    }
+		// Might not actually be loaded, so we'll check in later.
+		check_groundhogg_active();
+	}
 }
 
+/**
+ * Check that Gorundhogg is using the latest available core version
+ *
+ * @return bool|int
+ */
+function check_core_version() {
 
+	$correct_version = version_compare( GROUNDHOGG_VERSION, GROUNDHOGG_EXTENSION_REQUIRED_CORE_VERSION, '>=' );
+
+	if ( ! $correct_version ) {
+		add_action( 'admin_notices', function () {
+			$message      = sprintf( esc_html__( '%s requires Groundhogg version %s+. Because you are using an earlier version, the plugin is currently NOT RUNNING.', 'groundhogg' ), GROUNDHOGG_EXTENSION_NAME, GROUNDHOGG_EXTENSION_REQUIRED_CORE_VERSION );
+			$html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
+			echo wp_kses_post( $html_message );
+		} );
+	}
+
+	return $correct_version;
+}
+
+/**
+ * Check that the wp version is most recent
+ *
+ * @return bool|int
+ */
+function check_wp_version() {
+
+	$correct_version = version_compare( get_bloginfo( 'version' ), GROUNDHOGG_EXTENSION_REQUIRED_WP_VERSION, '>=' );
+
+	if ( ! $correct_version ) {
+		add_action( 'admin_notices', function () {
+			$message      = sprintf( esc_html__( '%s requires WordPress version %s+. Because you are using an earlier version, the plugin is currently NOT RUNNING.', 'groundhogg' ), GROUNDHOGG_EXTENSION_NAME, GROUNDHOGG_EXTENSION_REQUIRED_WP_VERSION );
+			$html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
+			echo wp_kses_post( $html_message );
+		} );
+	}
+
+	return $correct_version;
+}
+
+/**
+ * Check that the PHP version is compatible
+ *
+ * @return bool|int
+ */
+function check_php_version() {
+
+	$correct_version = version_compare( PHP_VERSION, GROUNDHOGG_EXTENSION_REQUIRED_PHP_VERSION, '>=' );
+
+	if ( ! $correct_version ) {
+		add_action( 'admin_notices', function () {
+			$message      = sprintf( esc_html__( '%s requires PHP version %s+, plugin is currently NOT RUNNING.', 'groundhogg' ), GROUNDHOGG_EXTENSION_NAME, GROUNDHOGG_EXTENSION_REQUIRED_PHP_VERSION );
+			$html_message = sprintf( '<div class="notice notice-error">%s</div>', wpautop( $message ) );
+			echo wp_kses_post( $html_message );
+		} );
+	}
+
+	return $correct_version;
+}
+
+/**
+ * Check that Groundhogg is active!
+ */
+function check_groundhogg_active() {
+	// Might not actually be loaded, so we'll check in later.
+	add_action( 'admin_notices', function () {
+
+		// Is not loaded!
+		if ( ! defined( 'GROUNDHOGG_VERSION' ) ) {
+			$message      = sprintf( esc_html__( 'Groundhogg is not currently active, it must be active for %s to work.', 'groundhogg' ), GROUNDHOGG_EXTENSION_NAME );
+			$html_message = sprintf( '<div class="notice notice-warning">%s</div>', wpautop( $message ) );
+			echo wp_kses_post( $html_message );
+		}
+	} );
+}
